b0d2bb5f90c6a5c0a070fa46eda16e1bd473b3fd
krzysztof.babik (e00205005) <krzysztof.babik@globallogic.com>
2025-03-21 13:39:43 +0100

    Implemented Checkboxes tests

---
+++ HerokuappTests/Pages/CheckboxesPage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Playwright;
+
+namespace HerokuappTests.Pages
+{
+    public class CheckboxesPage
+    {
+        public readonly IPage _page;
+        public readonly ILocator Checkbox;
+        public CheckboxesPage(IPage page)
+        {
+            _page = page;
+            Checkbox = _page.Locator("input[type='checkbox']");
+        }
+
+        public async Task NavigateToUrlAsync()
+        {
+            await _page.GotoAsync("https://the-internet.herokuapp.com/checkboxes");
+        }
+
+        public async Task<int> GetCheckboxesCount()
+        {
+            return await Checkbox.CountAsync();
+        }
+        public async Task<bool> GetCheckboxState(int idx)
+        {
+            bool isChecked = await Checkbox.Nth(idx).IsCheckedAsync();
+            return isChecked;
+        }
+
+        public async Task ChangeCheckboxState(int idx)
+        {
+            await Checkbox.Nth(idx).ClickAsync();
+        }
+    }
+}
---
+++ HerokuappTests/Tests/CheckboxesTests.cs
@@ -0,0 +1,65 @@
+﻿using HerokuappTests.Helpers;
+using HerokuappTests.Pages;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuappTests.Tests
+{
+    [TestFixture]
+    [Category("Herokuapp")]
+    public class CheckboxesTests
+    {
+        private PlaywrightSetup _playwright;
+        private IBrowserContext _context;
+        private IPage _page;
+        private CheckboxesPage _checkboxesPage; 
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _playwright = new PlaywrightSetup();
+            await _playwright.InitializeAsync();
+            _context = await _playwright.CreateNewContextAsync();
+            _page = await _context.NewPageAsync();
+
+            _checkboxesPage = new CheckboxesPage(_page);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _page.CloseAsync();
+        }
+
+        [Test]
+        public async Task Checkboxes01_CheckboxCount()
+        {
+            await _checkboxesPage.NavigateToUrlAsync();
+
+            var counter = await _checkboxesPage.GetCheckboxesCount();
+            Assert.That(2, Is.EqualTo(counter), "Number of checkboxes are wrong");
+        }
+
+        [Test]
+        public async Task Checkboxes02_ChangeCheckboxState()
+        {
+            await _checkboxesPage.NavigateToUrlAsync();
+
+            int checkboxCounter = await _checkboxesPage.GetCheckboxesCount();
+            for (int idx = 0; idx < checkboxCounter; idx++)
+            {
+                var initState = await _checkboxesPage.GetCheckboxState(idx);
+                await _checkboxesPage.ChangeCheckboxState(idx);
+                var newState = await _checkboxesPage.GetCheckboxState(idx);
+
+                Assert.That(initState, Is.Not.EqualTo(newState));
+            }
+        }
+    }
+}
