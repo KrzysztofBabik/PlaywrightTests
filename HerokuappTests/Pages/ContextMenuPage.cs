d3d32cee1025859af2471066513da95658ae9859
krzysztof.babik (e00205005) <krzysztof.babik@globallogic.com>
2025-03-24 12:26:24 +0100

    Implemented ContextMenu tests

---
+++ HerokuappTests/Pages/ContextMenuPage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuappTests.Pages
+{
+    public class ContextMenuPage
+    {
+        private IPage _page;
+        public ContextMenuPage(IPage page)
+        {
+            _page = page;
+        }
+
+        ILocator HotSpot => _page.Locator("#hot-spot");
+
+        public async Task GoToUrlAsync()
+        {
+            await _page.GotoAsync("https://the-internet.herokuapp.com/context_menu");
+        }
+        public async Task RighClickHotSpot()
+        {
+            await HotSpot.ClickAsync(new() { Button = MouseButton.Right });
+        }
+    }
+}
---
+++ HerokuappTests/Tests/ContextMenuTests.cs
@@ -0,0 +1,48 @@
+﻿using HerokuappTests.Helpers;
+using HerokuappTests.Pages;
+using Microsoft.Playwright;
+using Microsoft.VisualBasic;
+using NUnit.Framework;
+
+namespace HerokuappTests.Tests
+{
+    [TestFixture]
+    [Category("Herokuapp")]
+    public class ContextMenuTests
+    {
+        private PlaywrightSetup _playwright;
+        private IBrowserContext _context;
+        private IPage _page;
+        private ContextMenuPage _contextMenuPage;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _playwright = new PlaywrightSetup();
+            await _playwright.InitializeAsync();
+            _context = await _playwright.CreateNewContextAsync();
+            _page = await _context.NewPageAsync();
+            
+            _contextMenuPage = new ContextMenuPage(_page);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _page.CloseAsync();
+        }
+
+        [Test]
+        public async Task ContextMenu01_CheckJavaScriptAlert()
+        {
+            await _contextMenuPage.GoToUrlAsync();
+
+            _page.Dialog += async (_, dialog) =>
+            {
+                Assert.That(dialog.Message, Is.EqualTo("You selected a context menu"));
+                await dialog.AcceptAsync();
+            };
+            await _contextMenuPage.RighClickHotSpot();
+        }
+    }
+}
